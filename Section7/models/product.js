f290033d51d6ee247a02809a5f18315ffeee3bf0
Abhishek Dethe <abhishekdethe7066@gmail.com>
2024-09-13 00:14:58 +0530

    final changes

--- Section7/models/product.js
+++ Section7/models/product.js
@@ -1,42 +1,38 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
+
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err || fileContent.length === 0) {
+            return cb([]); // Handle error or empty file
+        }
+        try {
+            cb(JSON.parse(fileContent));
+        } catch (error) {
+            console.log('Error parsing JSON in fetchAll:', error);
+            return cb([]); // Return empty array if parsing fails
+        }
+    });
+}
+
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json'); // adding data in products.json in 'data' folder
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err  && fileContent.length > 0) {
-                try {
-                    products = JSON.parse(fileContent);
-                } catch (error) {
-                    console.log('Error parsing JSON:', error);
-                }
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log('Error writing file:', err);
             });
-        });
+        })
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
-        fs.readFile(p, (err, fileContent) => {
-            if (err || fileContent.length === 0) {
-                cb([]); // Handle error or empty file
-            }
-            try {
-                cb(JSON.parse(fileContent));
-            } catch (error) {
-                console.log('Error parsing JSON in fetchAll:', error);
-                cb([]); // Return empty array if parsing fails
-            }
-        });
+        getProductsFromFile(cb);
     }
 };
 
